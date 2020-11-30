533f950174ba58965a0906425d86741281ebca8d
prashant kumar <pkumars397@gmail.com>
2020-11-30 23:41:17 +0530

    imported javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("script is working from different file")
+//input 
+var userName=prompt("enter user name");
+// processing
+var welcomeMsg="This script works well "+userName;
+// output
+alert(welcomeMsg);
+
