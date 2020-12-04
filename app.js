05d6da72b4b4abf44f4ca6885841b8295bd23e9a
prashant kumar <pkumars397@gmail.com>
2020-12-05 03:52:22 +0530

    upto error handling

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var btnTranslate=document.querySelector("#btn-translate");
 
-var txtButton=document.querySelector("#txt-area");
+var txtInput=document.querySelector("#txt-input");
 
 var outputDiv=document.querySelector("#out")
 
-function clickEventHandler(){
-  outputDiv.innerText="prahsantl"+txtButton.value;
+var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslatedUrl(text){
+    return serverUrl+"?"+"text"+text;
 }
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server");
+}
+
+function clickEventHandler(){
+    var inputText=txtInput.value;
+
+    fetch(getTranslatedUrl(inputText))
+     .then(Response => Response.json())
+     .then(json => console.log(json.contents.translated))
+     .catch(errorHandler);
+};
 
 btnTranslate.addEventListener("click",clickEventHandler);
